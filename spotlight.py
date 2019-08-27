fbc5777abb6b2392630e73cd88b0b1afdae49ac3
Mark Parmelee <mparm920@pop-os.localdomain>
2019-08-27 12:37:11 -0500

    turned into a class

--- spotlight.py
+++ spotlight.py
@@ -3,18 +3,25 @@ import requests
 import configparser
 from datetime import datetime
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+class SpotLightTimer
 
-api_url = '{0}weather?zip=48462,us{1}'.format(config['API']['API_BASE_URL'],
-    config['API']['API_TOKEN'])
+    def __init__():
+        config = configparser.ConfigParser()
+        config.read('config.ini')
 
-res = requests.get(api_url)
-if res.status_code == 200:
-    sunrise = json.loads(res.content.decode('utf8'))['sys']['sunrise']
-    sunset = json.loads(res.content.decode('utf-8'))['sys']['sunset']
-    print('sunrise: {0}'.format(datetime.utcfromtimestamp(sunrise)))
-    print('sunset {0}'.format(datetime.utcfromtimestamp(sunset)))
-    #print(json.loads(res.content.decode('utf-8')))
-else:
-    print(res.status_code)
+        self.api_url = '{0}weather?zip=48462,us{1}'.format(config['API']['API_BASE_URL'],
+        config['API']['API_TOKEN'])
+
+    def requestTime():
+        res = requests.get(self.api_url)
+        if res.status_code == 200:
+            sunrise = json.loads(res.content.decode('utf8'))['sys']['sunrise']
+            sunset = json.loads(res.content.decode('utf-8'))['sys']['sunset']
+            print('sunrise: {0}'.format(datetime.utcfromtimestamp(sunrise)))
+            print('sunset {0}'.format(datetime.utcfromtimestamp(sunset)))
+            #print(json.loads(res.content.decode('utf-8')))
+        else:
+            print(res.status_code)
+
+if __name__ == '__main__':
+    SpotLightTimer()
