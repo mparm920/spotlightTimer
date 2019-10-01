c6a229284e45c99b8095fc560353c0cf20807ca0
Mark Parmelee <mparm920@gmail.com>
2019-09-30 23:55:01 -0400

    trying to setup global logging option

---
+++ __init__.py
@@ -0,0 +1,6 @@
+import logging
+from datetime import datetime
+from models import models
+
+logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(asctime)s %(message)s")
+log = logging.getLogger()
--- spotlight.py
+++ spotlight.py
@@ -1,37 +1,35 @@
 import json
 import requests
 import configparser
-import logging
+import log
 from datetime import datetime
 from models import models
 
 class SpotLightTimer:
+
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        self.api_url = '{0}weather?zip=48462,us{1}'.format(config['API']['API_BASE_URL'],
+        config['API']['API_TOKEN'])
 
     def logTimes(self, checkTime, sunrise, sunset):
         t = models.Timer(checkTime, sunrise, sunset)
         models.session.add(t)
         models.session.commit()
 
-    def __init__(self):
-        config = configparser.ConfigParser()
-        config.read('config.ini')
-
-        self.api_url = '{0}weather?zip=48462,us{1}'.format(config['API']['API_BASE_URL'],
-        config['API']['API_TOKEN'])
-
     def requestTime(self):
         res = requests.get(self.api_url)
         if res.status_code == 200:
-            sunrise = json.loads(res.content.decode('utf8'))['sys']['sunrise']
-            sunset = json.loads(res.content.decode('utf-8'))['sys']['sunset']
-            checkTime = datetime.now()
-            self.logTimes(checkTime, sunrise, sunset)
-            logging.debug('sunrise: {0}'.format(datetime.utcfromtimestamp(sunrise)))
-            logging.debug('sunset {0}'.format(datetime.utcfromtimestamp(sunset)))
+            sunriseUnix = json.loads(res.content.decode('utf8'))['sys']['sunrise']
+            sunsetUnix = json.loads(res.content.decode('utf-8'))['sys']['sunset']
+            sunrise = datetime.utcfromtimestamp(sunriseUnix)
+            sunset = datetime.utcfromtimestamp(sunsetUnix)
+            log.debug('sunrise: {0}'.format(sunrise))
+            log.debug('sunset {0}'.format(sunset))
+            return (sunriseUnix, sunsetUnix)
         else:
             print(res.status_code)
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(asctime)s %(message)s")
-    light = SpotLightTimer()
-    light.requestTime()
+    SpotLightTimer()
